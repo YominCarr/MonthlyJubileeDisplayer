686a1e9aa8d9df0a0665f92e902433cdb96c860a
YominCarr <alex.arth@googlemail.com>
2019-01-13 11:44:28 +0100

    Changed appearance of buttons to wp standard

--- includes/class-mjd-table.php
+++ includes/class-mjd-table.php
@@ -166,7 +166,7 @@ class MjdTable {
 			$html .= "<td>" . $dataRow["gender"] . "</td>";
 			$html .= "<td>" . $dataRow["birthday"] . "</td>";
 			$html .= "<td>" . $dataRow["residence"] . "</td>";
-			$html .= "<td><input type='button' value='Delete' /></td>";
+			$html .= "<td><input type='button' class='button button-primary' value='Delete' /></td>";
 			$html .= "</tr>";
 		}
 
@@ -175,7 +175,7 @@ class MjdTable {
 		$html .= "<td><select id='jubileeNewGender'><option>m</option><option>f</option></select></td>";
 		$html .= "<td><input type='date' id='jubileeNewBirthday' /></td>";
 		$html .= "<td><input type='text' id='jubileeNewResidence' /></td>";
-		$html .= "<td><input type='button' value='Insert' /></td>";
+		$html .= "<td><input type='button' class='button button-primary' value='Insert' /></td>";
 		$html .= "</tr>";
 
 		$html .= "</table>";
